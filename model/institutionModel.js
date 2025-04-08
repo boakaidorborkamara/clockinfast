bdab3cd887e29fd5478919d120b24a6b0dfd59ce
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2025-04-08 14:21:28 +0000

    restructure user schema and institution schema

--- model/institutionModel.js
+++ model/institutionModel.js
@@ -3,11 +3,6 @@ const mongoose = require("mongoose");
 // create schema
 const institutionSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true, maxLength: 20 },
-
-  email: { type: String, required: true, trim: true, maxLength: 20 },
-
-  password: { type: String, required: true, trim: true },
-
   logo: { type: String, required: true, trim: true },
 });
 
--- model/userModel.js
+++ model/userModel.js
@@ -2,24 +2,9 @@ const mongoose = require("mongoose");
 
 // create schema
 const userSchema = new mongoose.Schema({
-  first_name: { type: String, required: true, trim: true, maxLength: 15 },
-
-  middle_name: { type: String, trim: true },
-
-  last_name: { type: String, required: true, trim: true, maxLength: 15 },
+  name: { type: String, required: true, trim: true, maxLength: 15 },
 
   profile_image: { type: String, required: true, trim: true },
-
-  gender: {
-    type: String,
-    required: true,
-    enum: {
-      values: ["male", "female"],
-      message: "{VALUE} is not a valid gender",
-    },
-    lowercase: true,
-    trim: true,
-  },
 
   email: { type: String, required: true, trim: true, maxLength: 20 },
 
@@ -35,12 +20,6 @@ const userSchema = new mongoose.Schema({
 
   code: { type: String, required: true, trim: true },
 });
-
-// add method on schema that the full name of the user
-userSchema.methods.getFullName = function getFullName() {
-  const full_name = `${this.first_name} ${this.last_name}`;
-  return full_name;
-};
 
 // create model
 const User = mongoose.model("User", userSchema);
