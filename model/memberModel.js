28acc40098c31bd63e516e9b8765346fa97aa10d
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2025-04-08 14:33:08 +0000

    create member schema and model

--- model/memberModel.js
+++ model/memberModel.js
@@ -1,28 +1,23 @@
 const mongoose = require("mongoose");
 
 // create schema
-const userSchema = new mongoose.Schema({
+const memberSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true, maxLength: 15 },
 
   profile_image: { type: String, required: true, trim: true },
 
   email: { type: String, required: true, trim: true, maxLength: 20 },
 
+  code: { type: String, required: true, trim: true },
+
   password: { type: String, required: true, trim: true },
 
-  role: {
-    type: String,
-    enum: {
-      values: ["admin", "standard"],
-      message: "{VALUE} is not a valid role",
-    },
-    lowercase: true,
-  },
+  group_id: { type: ObjectId, required: true, trim: true },
 
   institution_id: { type: ObjectId, required: true, trim: true },
 });
 
 // create model
-const User = mongoose.model("User", userSchema);
+const Member = mongoose.model("Member", memberSchema);
 
-module.exports = { User };
+module.exports = { Member };
